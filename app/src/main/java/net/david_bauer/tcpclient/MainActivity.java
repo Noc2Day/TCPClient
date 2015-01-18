faf6d8a3c8e437b836bcbdb38ce08057dbccec31
David <github@david-bauer.net>
2015-01-18 23:16:56 +0100

    Added function to custom message

--- app/src/main/java/net/david_bauer/tcpclient/MainActivity.java
+++ app/src/main/java/net/david_bauer/tcpclient/MainActivity.java
@@ -1,35 +1,23 @@
 package net.david_bauer.tcpclient;
 //hello
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.Cursor;
-import android.media.audiofx.BassBoost;
 import android.os.Handler;
 import android.os.StrictMode;
-import android.provider.Telephony;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import org.apache.http.protocol.HTTP;
-import org.w3c.dom.NameList;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -37,9 +25,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -179,6 +164,11 @@ public class MainActivity extends ActionBarActivity {
             }
         });
         myList.deferNotifyDataSetChanged();
+    }
+
+    public void connect(View v) {
+        EditText et = (EditText) findViewById(R.id.send_message_edittext);
+        connect(et.getText().toString());
     }
 
     public void connect(String msg) {
